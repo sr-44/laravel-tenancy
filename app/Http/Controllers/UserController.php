417b1e7a5cb89897940a2404629f49cf6e90e5f1
sr-44 <mail.sr.44@gmail.com>
2024-01-24 17:25:15 +0500

    invitation...

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\Models\Invitation;
 use Illuminate\Http\Request;
+use Str;
 
 class UserController extends Controller
 {
     public function index()
     {
         return view('users.index');
+    }
+
+    public function store(StoreUserRequest $request)
+    {
+        $invitation = Invitation::create([
+            'tenant_id' => auth()->user()->current_tenant_id,
+            'email' => $request->email,
+            'token' => Str::random(40),
+        ]);
+
+        return redirect()->route('users.index')
+            ->with('success', 'Invitation sent!');
     }
 }
